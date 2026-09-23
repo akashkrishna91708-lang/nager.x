ba930c9777bc2b22bafb1511b0fd280b87600cad
Vivek Patel <vivekpatel181205@gmail.com>
2026-09-23 14:47:35 +0530

    Make local app deployable and LAN accessible

--- server/server.js
+++ server/server.js
@@ -87,4 +87,6 @@ if (fs.existsSync(clientDist)) {
   app.use(express.static(clientDist));
   app.get('*', (req, res, next) => req.path.startsWith('/api/') ? next() : res.sendFile(path.join(clientDist, 'index.html')));
 }
-app.listen(process.env.PORT||5000, '127.0.0.1', ()=>console.log(`NagarX API running on http://localhost:${process.env.PORT||5000}`));
+const port = Number(process.env.PORT || 5000);
+const host = process.env.HOST || '0.0.0.0';
+app.listen(port, host, ()=>console.log(`NagarX API running on http://localhost:${port} and available on the local network`));
